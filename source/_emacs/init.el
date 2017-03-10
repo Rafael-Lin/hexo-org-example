7f698f544fcd526ce4cf3f52e37f244e64623a27
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-03-11 00:31:09 +0800

    make emacs install yaml-mode
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ source/_emacs/init.el
@@ -0,0 +1,27 @@
+;;; init.el
+
+;; Copyright (c) 2017 Yen-Chin, Lee.
+;;
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+
+(defvar config-path
+  (file-name-directory (or load-file-name (buffer-file-name))))
+
+;; install yaml-mode for syntax highlight
+(package-install 'yaml-mode)
